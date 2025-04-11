390ea38835ef844dbef9ebb8cb665c0908d6f23e
Regie Ramos <markramosregie19@gmail.com>
2025-04-11 11:12:27 +0800

    test

--- test_app.py
+++ test_app.py
@@ -1,12 +1,8 @@
-
-
-
-from app import add, subtract
-
+from app import add,subtract
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2,3)==5
 
 
 def test_subtract():
-    assert subtract(5, 3) == 2
+    assert subtract(5,3)==2
